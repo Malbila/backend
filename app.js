7d78db57abc918633200d8ca0696aef6856b587b
Malbila <smalbila07@gmail.com>
2022-01-20 14:56:03 +0000

    new commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const Thing = require('./models/thing')
-const thing = require('./models/thing')
+const stuffRoutes = require('./routes/stuff')
 
 const app = express()
 
@@ -20,36 +20,11 @@ app.use((req, res, next) => {
     next();
 })
 
+app.use('/api/stuff', stuffRoutes)
+
 app.use(bodyParser.json())
 
 
-app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-      ...req.body
-    })
-    thing.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }))
-})
 
-app.get('/api/stuff/:id', (req, res, next) => {
-  Thing.findOne({ _id: req.params.id})
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(400).json({ error }))
-})
-
-app.put('/api/stuff/:id', (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-  .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-  .catch(error => res.status(400).json({ error }))
-})
-
-
-app.get('/api/stuff', (req, res, next) => {
-  Thing.find()
-  .then(things => res.status(200).json(things))
-  .catch(error => res.status(404).json({ error }))
-  });
 
 module.exports = app
